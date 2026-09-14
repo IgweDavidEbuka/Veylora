95ebbb50c83f7bdd48913fa55dc540cd6e6bc02b
Ebuka Igwe <ebuka.igwe@stu.cu.edu.ng>
2026-09-14 16:39:42 +0100

    Version 0.0.5: Zone camps named and randomizer for camps made

--- game/zones.py
+++ game/zones.py
@@ -8,36 +8,36 @@ class ZoneType(Enum):
     CASTLE = 3
 
 class Zone:
-    CAMP_POOL : dict[elements.Element, dict[str, list]] = {
+    CAMP_POOL : dict[elements.Element, dict[int, list]] = {
         elements.Element.LIGHT: {
-            "Tier 1": [],
-            "Tier 2": [],
-            "Tier 3": []
+            1 : ["Elarion", "Phos Repostory", "Luminara"],
+            2 : ["Krias", "Sunspire"],
+            3 : ["Sanctum Aurum", "Elyndor"]
         },
         elements.Element.SHADOW: {
-            "Tier 1": [],
-            "Tier 2": [],
-            "Tier 3": []
+            1 : ["Duskwell", "Veyra", "Morevane"],
+            2 : ["Blackreach", "Drazheil"],
+            3 : ["Veynhaleth", "Zhaevor"]
         },
         elements.Element.WATER: {
-            "Tier 1": [],
-            "Tier 2": [],
-            "Tier 3": []
+            1 : ["Nerevia", "Ocevaris", "Thasslyr"],
+            2 : ["The Sunken Ark", "Nerevia"],
+            3 : ["Aetherian Trench", "Crystalline Cenotaph"]
         },
         elements.Element.WIND: {
-            "Tier 1": [],
-            "Tier 2": [],
-            "Tier 3": []
+            1 : ["Windreach", "Aereth", "Aeralis"],
+            2 : ["Caelvyr", "Mount Avarra"],
+            3 : ["Heaven's Crest", "Nimbus Stratum"]
         },
         elements.Element.EARTH: {
-          "Tier 1": [],
-          "Tier 2": [],
-          "Tier 3": []  
+            1 : ["Aderra", "Durnak", "Eldara"],
+            2 : ["Morundra", "Lithurila"],
+            3 : ["Tectonic Tarbanacle", "Bedrock Vestige"]  
         },
         elements.Element.CHAOS: {
-            "Tier 1": [],
-            "Tier 2": [],
-            "Tier 3": []
+            1 : ["Zyrexhalitha", "Ezkara", "Veznaria"],
+            2 : ["Vharaxis", "Orovex"],
+            3 : ["Xaevoryn", "Xaltherian Pit"]
         }
     }
         
@@ -46,4 +46,9 @@ class Zone:
         self.zone_type = zone_type
         self.tier = tier
     
-    
+    def camp_randomizer(self, element):
+        return random.choice(self.CAMP_POOL[element][random.randint(1, 3)])
+
+
+se = Zone('', '')
+print(se.camp_randomizer(elements.Element.CHAOS))
